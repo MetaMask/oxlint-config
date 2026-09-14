9ad70b24c4690eda3852acc033305590859ae22b
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2026-09-14 15:14:48 +0200

    Align Node.js config with Node.js ESLint config

--- packages/nodejs/src/index.js
+++ packages/nodejs/src/index.js
@@ -1,11 +1,16 @@
 import { createConfig } from '@metamask/oxlint-config';
 
 const config = createConfig({
-  plugins: ['node'],
+  plugins: ['node', 'unicorn'],
+
+  // `eslint-plugin-n` is loaded as a JS plugin to enable rules not yet
+  // natively implemented in Oxlint.
+  jsPlugins: ['eslint-plugin-n'],
 
   rules: {
     // Possible Errors
     'node/handle-callback-err': ['error', '^(err|error)$'],
+    'node/no-exports-assign': 'error',
     'node/no-new-require': 'error',
     'node/no-path-concat': 'error',
 
@@ -16,6 +21,31 @@ const config = createConfig({
     'node/no-mixed-requires': 'error',
     'node/no-process-env': 'error',
     'node/no-sync': 'error',
+
+    // Unicorn rules
+    'unicorn/no-process-exit': 'error',
+
+    // `eslint-plugin-n` rules
+    'n/hashbang': 'error',
+    'n/no-callback-literal': 'error',
+    'n/no-deprecated-api': 'error',
+    'n/no-extraneous-import': 'error',
+    'n/no-extraneous-require': 'error',
+    'n/no-unpublished-bin': 'error',
+    'n/no-unpublished-import': 'error',
+    'n/no-unpublished-require': 'error',
+    'n/no-unsupported-features/es-builtins': 'error',
+    'n/no-unsupported-features/node-builtins': 'error',
+    'n/prefer-global/buffer': 'error',
+    'n/prefer-global/console': 'error',
+    'n/prefer-global/process': 'error',
+    'n/prefer-global/text-decoder': 'error',
+    'n/prefer-global/text-encoder': 'error',
+    'n/prefer-global/url': 'error',
+    'n/prefer-global/url-search-params': 'error',
+    'n/prefer-promises/dns': 'error',
+    'n/prefer-promises/fs': 'error',
+    'n/process-exit-as-throw': 'error',
 
     // Enabled in the base config, but this should be allowed in Node.js
     // projects.
